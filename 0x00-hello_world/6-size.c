6d72778b6e6366a2e5e2247c3a4ed7c8067d677f
Mouhssine Bouincha <bouinchamouhssine@gmail.com>
2023-03-17 04:28:40 -0700

    C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Enrty point
+ *
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	int charType;
+	int intType;
+	int longintType;
+	int longlongType;
+
+	printf("Size of chat: zu% bytes\n", sizeof(charType));
+	printf("Size of int: zu% bytes\n", sizeof(inType));
+	printf("Size of long int: zu% bytes\n", sizeoef(longintType));
+	printf("Size of long long: zu% bytes\n", sizeof(longlongtypr));
+	return (0);
+}
