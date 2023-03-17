c4908fd7e5418c2dce24c3e74635bdd70eaef4a7
Mouhssine Bouincha <bouinchamouhssine@gmail.com>
2023-03-17 03:26:22 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
  * main - Entry point 
  *
@@ -8,7 +6,7 @@
  */
 int main(void)
 {
-printf("Programming is like building a multilingual puzzle,\n);
+puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+return (0);
 }
