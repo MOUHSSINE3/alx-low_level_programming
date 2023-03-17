82b1459a92cd381347c3283e25c20462870f8ed8
Mouhssine Bouincha <bouinchamouhssine@gmail.com>
2023-03-17 02:37:44 -0700

    C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main - Entry a ligne with using printf function;
+ * Return: 0 (success);
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art",\n);
+	return (0);
+}
