f34ffbaba3c5b569f8a208cf3092397ce7808588
Mouhssine Bouincha <bouinchamouhssine@gmail.com>
2023-03-19 04:52:25 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ int main (void)
 	int longintType;
 	int longlongType;
 
-	printf("Size of chat: zu% bytes\n", sizeof(charType));
-	printf("Size of int: zu% bytes\n", sizeof(inType));
-	printf("Size of long int: zu% bytes\n", sizeoef(longintType));
-	printf("Size of long long: zu% bytes\n", sizeof(longlongtypr));
+	printf("Size of chat: zu% byte(s)\n", sizeof(charType));
+	printf("Size of int: zu% byte(s)\n", sizeof(inType));
+	printf("Size of long int: zu% byte(s)\n", sizeof(longintType));
+	printf("Size of long long: zu% byte(s)\n", sizeof(longlongtypr));
 	return (0);
 }
