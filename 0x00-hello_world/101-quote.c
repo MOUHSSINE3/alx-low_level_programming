9c5623abc8287d32b052dfa8c4da532cb519153b
Mouhssine Bouincha <bouinchamouhssine@gmail.com>
2023-03-19 11:16:23 -0700

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 /**
  * main - Entry point
  *
- * Return: 1 (Success)
- * 
+ * Return 1 (Success)
  */
+
 int main(void)
 {
-	fput("and that piece of art is usefl\" - Dora Korpar, 2015-10-19\n",stdout);
+	fput("and that piece of art is usefl\" - Dora Korpar, 2015-10-19\n", stdout);
 		return (1);
 
 }
