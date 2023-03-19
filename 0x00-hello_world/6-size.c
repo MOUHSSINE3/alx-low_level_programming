eec10d8eece8bec6011a8b157805a2aecc77df44
Mouhssine Bouincha <bouinchamouhssine@gmail.com>
2023-03-19 05:21:19 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,16 +4,16 @@
  *
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char X;
 	int Y;
 	long int W;
 	long int Z;
 
-	printf("Size of char: zu% byte(s)\n", (unsigned long)sizeof(X));
-	printf("Size of int: zu% byte(s)\n", (unsigned long)sizeof(Y));
-	printf("Size of long int: zu% byte(s)\n", (unsigned long)sizeof(W));
-	printf("Size of long long: zu% byte(s)\n", (unsigned long)sizeof(Z));
+	printf("Size of a char: zu% byte(s)\n", (unsigned long)sizeof(X));
+	printf("Size of an int: zu% byte(s)\n", (unsigned long)sizeof(Y));
+	printf("Size of a long int: zu% byte(s)\n", (unsigned long)sizeof(W));
+	printf("Size of a long long: zu% byte(s)\n", (unsigned long)sizeof(Z));
 	return (0);
 }
