aa3b5a39c5143196b517d5f6a14b79001a90a943
Mouhssine Bouincha <bouinchamouhssine@gmail.com>
2023-03-19 06:24:20 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,13 @@ int main(void)
 	char X;
 	int Y;
 	long int W;
-	long long  int Z;
+	long long int Z;
+	float Q;
 
 	printf("Size of a char: zu% byte(s)\n", (unsigned long)sizeof(X));
 	printf("Size of an int: zu% byte(s)\n", (unsigned long)sizeof(Y));
 	printf("Size of a long int: zu% byte(s)\n", (unsigned long)sizeof(W));
 	printf("Size of a long long int: zu% byte(s)\n", (unsigned long)sizeof(Z));
+	printf("Size of a float: zu% bytes(s)\n", (unsigned long)sizeof(Q))
 	return (0);
 }
