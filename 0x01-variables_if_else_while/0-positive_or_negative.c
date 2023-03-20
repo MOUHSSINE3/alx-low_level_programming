19141693e29e97f03f21c28cc8089133a1d88140
Mouhssine Bouincha <bouinchamouhssine@gmail.com>
2023-03-20 15:00:57 -0700

    C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,7 +16,13 @@ int main(void)
 	print("%d is positive", n);
 	}
 	else if (n == 0)
+	{
 		print("%d is zero", n);
+	}
+	else
+	{
+	print("%d is negative", n)
+	}
 	/* your code goes there */
 	return (0);
 }
